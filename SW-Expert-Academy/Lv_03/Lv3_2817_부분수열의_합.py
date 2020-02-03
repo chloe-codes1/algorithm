efbb68915b08a0522c7a338f5d51417919c738ca
Chloe Kim <juhyun.kim@lindsey.edu>
2020-02-04 06:35:50 +0900

    First commit on 0204

--- SW-Expert-Academy/Lv_03/Lv3_2817_부분수열의_합.py
+++ SW-Expert-Academy/Lv_03/Lv3_2817_부분수열의_합.py
@@ -14,7 +14,7 @@
 
 # 각 테스트 케이스마다 ‘#x ’(x는 테스트케이스 번호를 의미하며 1부터 시작한다)를 출력하고, 부분 수열의 합이 K가 되는 경우의 수를 출력한다.
 
-import random
+import itertools
 
 T = int(input())
 
@@ -24,11 +24,10 @@ for t in range(1, T+1):
     array = list(map(int, input().split()))
 
     count = 0
-    for i in range(2, N):
-        for n in range(N * (N-1) * (N-2)):    
-            if sum(list(random.sample(array, i))) == K:
-                count+=1
-                break
-                
+    for i in range(1, N+1):
+        arrays = itertools.combinations(array, i)
+        for j in arrays:
+            if sum(j) == K:
+                count+=1                   
 
-    print(count)
+    print('#{} {}'.format(t,count))
